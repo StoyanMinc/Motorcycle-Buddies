904f94bdd2bd64520a509e56e1f54fd51776597f
Stoyan Minchev <154098583+StoyanMinc@users.noreply.github.com>
2025-03-23 21:54:57 +0200

    Fix GuestGuard

--- client/src/components/guards/GuestGuard.jsx
+++ client/src/components/guards/GuestGuard.jsx
@@ -1,4 +1,4 @@
-import { Navigate } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 import { getUser } from "../../context/AuthContext";
 
 export default function GuestGuard() {
